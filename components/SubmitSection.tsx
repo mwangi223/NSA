0210a7871055168ff1dfb3d5c6570e306d6e649a
mwangimuringi <mwangimuringi10@gmail.com>
2025-01-05 11:12:51 +0300

    Implemented missing attributes on SubmitButton.

--- components/SubmitSection.tsx
+++ components/SubmitSection.tsx
@@ -2,7 +2,7 @@ import SubmitButton from "@/components/SubmitButton";
 
 const SubmitSection = ({ isLoading }: { isLoading: boolean }) => (
   <section className="flex justify-end gap-2 pt-8">
-    <SubmitButton isLoading={isLoading} />
+    <SubmitButton isLoading={isLoading} children={undefined} />
   </section>
 );
 
