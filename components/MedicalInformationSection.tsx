b3411fbd746c8a9500815f2193ae6f0e5b818df7
mwangimuringi <mwangimuringi10@gmail.com>
2025-01-05 11:01:35 +0300

    Added MedicalInformationSection component for medical details.

---
+++ components/MedicalInformationSection.tsx
@@ -0,0 +1,31 @@
+// MedicalInformationSection.tsx
+import { Control } from "react-hook-form";
+import { FormFieldType } from "@/types";
+import CustomFormField from "@/components/CustomFormField";
+import { Label } from "@/components/ui/label";
+
+const MedicalInformationSection = ({ control }: { control: Control }) => (
+  <section className="space-y-6">
+    <div className="mb-9 space-y-1">
+      <h2 className="sub-header">Medical Information</h2>
+    </div>
+
+    <div>
+      <Label htmlFor="primaryPhysician">Primary care physician</Label>
+      <CustomFormField id="primaryPhysician" fieldType={FormFieldType.SELECT} control={control} name="primaryPhysician" placeholder="Select a physician" />
+    </div>
+
+    <div className="flex flex-col gap-6 xl:flex-row">
+      <div>
+        <Label htmlFor="insuranceProvider">Insurance provider</Label>
+        <CustomFormField id="insuranceProvider" fieldType={FormFieldType.INPUT} control={control} name="insuranceProvider" placeholder="SHIF" />
+      </div>
+      <div>
+        <Label htmlFor="insurancePolicyNumber">Insurance policy number</Label>
+        <CustomFormField id="insurancePolicyNumber" fieldType={FormFieldType.INPUT} control={control} name="insurancePolicyNumber" placeholder="ABC123456789" />
+      </div>
+    </div>
+  </section>
+);
+
+export default MedicalInformationSection;
